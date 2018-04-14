60f4a4b4e6a2838e42cfb1ec37084944c7b7cc9a
Jakub Wiśniewski <jwisniewski@blastlab.co>
2018-04-14 15:33:49 +0200

    endomondo service

---
+++ endo/src/main/java/co/blastlab/behalthy/model/Workout.java
@@ -0,0 +1,17 @@
+package co.blastlab.behalthy.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Workout {
+    private Double calories;
+    private Short sport;
+    private Double distance;
+    private Double speed_avg;
+    private Double duration;
+    private String start_time;
+}
